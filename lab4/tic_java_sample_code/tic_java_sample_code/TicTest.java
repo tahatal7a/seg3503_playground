57e7e1f1ca413decab98ffde5787e6e437ec235c
tahatal7a <ttalh092@uottawa.ca>
2023-06-29 19:13:09 -0400

    3rd test fail

--- lab4/tic_java_sample_code/tic_java_sample_code/TicTest.java
+++ lab4/tic_java_sample_code/tic_java_sample_code/TicTest.java
@@ -15,11 +15,18 @@ class TicTest {
 		assertEquals(board.equals(board2),  true);
 	}
 
-		@Test
+	@Test
     void testEmpty() {
         Tic board = new Tic(3, 3);
 
         assertEquals(board.isEmpty(), true);
     }
 
+	@Test
+    void testWinner() {
+        Tic board = new Tic(3, 3);
+        board.move(2, 1);
+        board.move(0, 1);
+        assertEquals(board.isWin(), true);
+    }
 }
